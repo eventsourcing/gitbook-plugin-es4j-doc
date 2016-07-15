d09c93c9479397f7f7cdcd04cd503125a2992969
Yurii Rashkovskii <yrashk@gmail.com>
2016-07-15 12:13:00 +0700

    If there is no matching SNAPSHOT, return the original version

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ module.exports = {
           }
           var xml = request('GET', 'http://dl.bintray.com/eventsourcing/maven-snapshots/com/eventsourcing/eventsourcing-core/maven-metadata.xml').getBody()
           var v1 = v.replace(/-SNAPSHOT$/,"");
-          v1 = v1 + xml.toString().match(new RegExp(v1 + '(-[0-9]+-g[a-f0-9]+)<\\/version>\\s*<\\/versions>'))[1];
-          cache[v] = v1;
-          return v1;
+          var last = xml.toString().match(new RegExp(v1 + '(-[0-9]+-g[a-f0-9]+)<\\/version>\\s*<\\/versions>'))
+          if (last === null) {
+            return v
+          } else {
+           v1 = v1 + last[1];
+            cache[v] = v1;
+            return "v" + v1;
+          }
       }
   }
 }
